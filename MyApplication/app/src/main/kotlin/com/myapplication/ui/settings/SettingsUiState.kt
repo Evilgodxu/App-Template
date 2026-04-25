d20acf0bcf745aac9f950b95d99fd7a61d60bd0c
江寒 <my2973257684@gmail.com>
2026-04-26 07:13:18 +0800

    Delete MyApplication directory

--- MyApplication/app/src/main/kotlin/com/myapplication/ui/settings/SettingsUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.myapplication.ui.settings
-
-data class SettingsUiState(
-    val isLoading: Boolean = true,
-    val themeMode: String = "system",
-    val language: String = "system",
-)
