6bb2ccf46f5c8d67424787f627723c1a3042436f
江寒 <my2973257684@gmail.com>
2026-04-26 05:27:18 +0800

    Update build.gradle.kts

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material.icons.extended)
 
